72860045c2b7385b1a89976d0fc137ed2d01c3f6
Diego Simon <dsimog01@estudiantes.unileon.es>
2021-11-16 00:02:14 +0100

    Fixed compilation error

--- client/src/components/RacketInput/RacketInput.js
+++ client/src/components/RacketInput/RacketInput.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import InputField from '../InputField';
+import InputField from '../InputField/InputField';
 import './RacketInput.css';
 
 class RacketInput extends Component {
--- client/src/components/TodoForm/TodoForm.js
+++ client/src/components/TodoForm/TodoForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import InputField from '../InputField';
+import InputField from '../InputField/InputField';
 
 class TodoForm extends Component {
     constructor() {
