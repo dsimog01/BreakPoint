dfb8fe70b09b14ecf422d26334d01a571a24941e
Diego Simon <dsimog01@estudiantes.unileon.es>
2021-11-25 00:57:00 +0100

    Fixed colors and RacketRater finished

--- client/src/components/Navigation/AuthNav.js
+++ client/src/components/Navigation/AuthNav.js
@@ -3,7 +3,7 @@ import AuthenticationButton from '../Login/AuthenticationButton';
 
 const AuthNav = () => (
   <div className="navbar-nav ml-auto">
-    <AuthenticationButton />
+    <AuthenticationButton/>
   </div>
 );
 
--- client/src/components/RacketRater/RacketRater.js
+++ client/src/components/RacketRater/RacketRater.js
@@ -16,9 +16,28 @@ class RacketRater extends Component {
         return(
             <div className="container mt-5 p-4 border rounded-3">
                 <h2> Do you want to rate other rackets?  </h2>  
-                <div className="container">
-                    <Select/>
-                    <button className="btn btn-primary">Rate it</button>
+                <div >
+                    <div id="selectors" className="row">
+                        <div id="brandSelector" class="col-md-6">
+                            <Select defaultValue={{ label: "Brand", value: "brand" }}/>
+                        </div>
+                        <div id="sizeSelector" class="col-md-6 mb-2">
+                            <Select defaultValue={{ label: "Head Size (in2)", value: "headSize_in2" }}/>
+                        </div>
+                        <div id="lengthSelector" class="col-md-6">
+                            <Select defaultValue={{ label: "Length (in)", value: "length" }}/>
+                        </div>
+                        <div id="weightSelector" class="col-md-6 mb-2">
+                            <Select defaultValue={{ label: "Weight (g)", value: "weight" }}/>
+                        </div>
+
+                    </div>
+
+                    <center><div id="rating" className="mb-2 w-25">
+                            <input type="text" class="form-control" placeholder="Rating"/>
+                    </div></center>
+
+                    <button className="btn btn-dark">Rate it</button>
                 </div>
             </div>
         );
